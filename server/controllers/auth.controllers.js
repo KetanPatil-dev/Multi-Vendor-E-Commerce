a98b4ce41234978632313799721a84ff0e19619d
ketan <mail.ketan027@gmail.com>
2025-04-14 12:35:06 +0530

    Implemented access and refreshtoken and will save refreshtoken token in redis database

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,4 +1,33 @@
+import { redis } from "../lib/redis.js"
 import UserModel from "../models/user.model.js"
+import jwt from "jsonwebtoken"
+
+const generateTokens=(userId)=>{
+    const accessToken=jwt.sign({userId},process.env.ACCESS_TOKEN,{expiresIn:"15m"})
+
+    const refreshToken= jwt.sign({userId},process.env.REFRESH_TOKEN,{expiresIn:"7d"})
+    return {accessToken,refreshToken}
+}
+
+const storeRefreshToken=async(userId,refreshToken)=>{
+    await redis.set(`refresh_token:${userId}`,refreshToken,"EX",7*24*60*60 )
+}
+const setCookies=(res,refreshToken,accessToken)=>{
+
+    res.cookie("accessToken",accessToken,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"strict" , //preventd CSRF attacks
+        maxAge:15*60*1000
+    })
+    res.cookie("refreshToken",refreshToken,{
+        httpOnly:true,
+        sameSite:"strict",
+        secure:false,
+        maxAge:7*24*60*60*1000
+    })
+
+}
 
 export const Signup=async(req ,res)=>{
     const {name,email,password}=req.body
@@ -11,8 +40,15 @@ export const Signup=async(req ,res)=>{
             return res.status(400).json({success:false,message:"User Already Exists"})
         }
 const newUser= new UserModel({name,email,password})
+
 await newUser.save()
-res.status(201).json({success:true,message:"User Created Successfully...",user:newUser})
+const {password:_,...userData}=newUser.toObject()
+const {accessToken,refreshToken}= generateTokens(newUser._id)
+await storeRefreshToken(newUser._id,refreshToken)
+setCookies(res,refreshToken,accessToken)
+res.status(201).json({success:true,message:"User Created Successfully...",userData
+    
+})
     } catch(error)
     {
         console.log("Signup Error",error)
