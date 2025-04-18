7a5c57bb19e834e5d9b87bb64801c926f1a49705
ketan <mail.ketan027@gmail.com>
2025-04-18 12:04:58 +0530

    Implemented Cart Routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { ConnectDB } from "./connect.js"
 import cookieParser from "cookie-parser"
 import AuthRoutes from "./routes/auth.routes.js"
 import ProductRoutes from "./routes/product.route.js"
+import CartRoutes from "./routes/cart.route.js"
 dotenv.config()
 
 const app=express()
@@ -17,6 +18,7 @@ const Start=async()=>{
        ConnectDB()
        app.use("/auth",AuthRoutes)
        app.use("/products",ProductRoutes)
+       app.use("/cart",CartRoutes)
        app.listen(PORT,()=>console.log(`Server Started on PORT:${PORT}`))
 
     } catch (error) {
