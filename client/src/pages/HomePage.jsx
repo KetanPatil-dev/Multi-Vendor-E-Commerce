c02eecb2170d49e023725ce3acd45003473503c5
ketan <mail.ketan027@gmail.com>
2025-04-22 13:57:50 +0530

    Implemented navigation and signupPage

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='text-blue-500 text-5xl'>HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+// LoginPage.jsx
+const LoginPage = () => {
+    return (
+      <div className="p-4">
+        <h1 className="text-3xl font-bold">Login Page</h1>
+        {/* Your login form or content here */}
+      </div>
+    );
+  };
+  
+  export default LoginPage;
+  
