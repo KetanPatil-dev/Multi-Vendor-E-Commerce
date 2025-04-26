8a7b3187b1ff876d0956be9aa60b0297fc8a9ac4
ketan <mail.ketan027@gmail.com>
2025-04-26 12:40:44 +0530

    impkemented CreateProductForm UI

---
+++ client/src/components/CreateProductsForm.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import {motion} from "framer-motion"
+import { PlusCircle,Upload,Loader } from 'lucide-react'
+
+const categories=["jean","t-shirt","show","glasses","jacket","suit","bag"]
+const CreateProductsForm = () => {
+    const[newproduct,setNewProduct]=useState({
+        name:"",
+        description:"",
+        price:"",
+        category:"",
+        image:""
+    });
+    const loading=false
+
+    const handleSubmit=(e)=>{
+e.preventDefault()
+console.log(newproduct)
+    }
+    const handleImageChange=(e)=>{
+      const file=e.target.files[0];
+      if(file)
+      {
+        const reader= new FileReader()
+        reader.onloadend=()=>{
+          setNewProduct({...newproduct,image:reader.result})
+        }
+        reader.readAsDataURL(file)//base64
+      }
+    }
+  return (
+    <motion.div className='bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto'>
+        <h2 className='text-2xl font-semibold text-center mt-6 text-emerald-400'>Create Product</h2>
+<form className='space-y-3' onSubmit={handleSubmit}>
+   <div> <label htmlFor='name' className='block text-sm font-medium text-gray-300'>Product Name</label>
+   <input id="name" type='text' name="name" value={newproduct.name} onChange={(e)=>setNewProduct({...newproduct,name:e.target.value})} className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500' required/></div>
+   <div> <label htmlFor='description' className='block text-sm font-medium text-gray-300'>Description</label>
+   <textarea id="description"  name="description" rows="3" value={newproduct.description} onChange={(e)=>setNewProduct({...newproduct,description:e.target.value})} className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500' required/></div>
+   <div> <label htmlFor='price' className='block text-sm font-medium text-gray-300'>Price</label>
+   <input id="price" type='number' name="price" value={newproduct.price} onChange={(e)=>setNewProduct({...newproduct,price:e.target.value})} className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500' required/></div>
+   <div>
+    <label htmlFor='category' className='block text-sm font-medium text-gray-300'>Category</label>
+    <select id='category' name='category' value={newproduct.category} onChange={(e)=>setNewProduct({...newproduct,category:e.target.value})} className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md shadow-sm py-2 px-3  text-white focus:outline-none focus:ring-2 focus:border-emerald-400'  required>
+ <option value="" >Select a category</option>
+ {categories.map((category,index)=>(
+  <option value={category} key={index+1}>{category}</option>
+ ))}
+ </select>
+   </div>
+   <div className='mt-1 flex items-center'>
+					<input type='file' id='image' className='sr-only' accept='image/*'onChange={handleImageChange} />
+					<label
+						htmlFor='image'
+						className='cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500'
+					>
+						<Upload className='h-5 w-5 inline-block mr-2' />
+						Upload Image
+					</label>
+					{newproduct.image && <span className='ml-3 text-sm text-gray-400 font-bold'>Image uploaded</span>}
+				</div>
+        <button
+					type='submit'
+					className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md 
+					shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 
+					focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50'
+					disabled={loading}
+				>
+					{loading ? (
+						<>
+							<Loader className='mr-2 h-5 w-5 animate-spin' aria-hidden='true' />
+							Loading...
+						</>
+					) : (
+						<>
+							<PlusCircle className='mr-2 h-5 w-5' />
+							Create Product
+						</>
+					)}
+				</button>
+
+</form>
+    </motion.div>
+  )
+}
+
+export default CreateProductsForm
---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { BarChart, PlusCircle, ShoppingCart } from "lucide-react";
+import CreateProductsForm from "../components/CreateProductsForm";
+import ProductsList from "../components/ProductsList";
+import AnalyticsTab from "../components/AnalyticsTab";
+
+const tabs = [
+  {
+    id: "create",
+    label: "Create Product",
+    icon: PlusCircle,
+  },
+  { id: "products", label: "Products", icon: ShoppingCart },
+  { id: "analytics", label: "Analytics", icon: BarChart },
+];
+
+const AdminPage = () => {
+  const [activeTab, setActiveTab] = useState("create");
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white overflow-hidden">
+      <div className="relative z-10 container mx-auto px-4 py-0">
+        <motion.h1
+          className="text-4xl text-center text-emerald-400 font-bold mb-6"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
+          Admin Dashboard
+        </motion.h1>
+
+        <motion.div className="flex justify-center flex-wrap mb-8 gap-4" initial={{opacity:0,y:20}} animate={{opacity:1,y:0}} transition={{duration:.8,delay:.2}}>
+          {tabs.map((tab) => (
+            <button
+              key={tab.id}
+              onClick={() => setActiveTab(tab.id)}
+              className={`flex items-center px-4 py-2 rounded-md transition-colors duration-200 ${
+                activeTab === tab.id
+                  ? "bg-emerald-600 text-white"
+                  : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+              }`}
+            >
+              <tab.icon className="mr-2 h-5 w-5" />
+              {tab.label}
+            </button>
+          ))}
+        </motion.div>
+
+       <motion.div initial={{opacity:0,y:20}} animate={{opacity:1,y:0}} transition={{duration:.8,delay:.2}}>{activeTab === "create" && <CreateProductsForm />}</motion.div> 
+        {activeTab === "products" && <ProductsList />}
+        {activeTab === "analytics" && <AnalyticsTab />}
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
